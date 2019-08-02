74ef39a515a609be828eaeb401455aa4f54127bf
Yuumi Yoshida <yy@ey-office.com>
2019-08-02 13:32:10 +0900

    Add FactoryBot.rewind_sequences

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -4,4 +4,9 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   # driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
   driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
   Webdrivers.cache_time = 1.week.to_i
+
+  def teardown
+    super
+    FactoryBot.rewind_sequences
+  end
 end
